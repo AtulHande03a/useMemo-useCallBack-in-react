39be27260278d331e4092f1c7405150abe995b6f
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-04 22:37:57 +0530

    added useCallbak

--- src/setup/index.js
+++ src/setup/index.js
@@ -10,6 +10,11 @@ const url = "https://course-api.com/javascript-store-products";
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
 
   return (
     <>
@@ -17,23 +22,34 @@ const Index = () => {
       <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>Cart:{cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   );
 };
 
-const BigList = ({ products }) => {
+//memo method in react cheks is the value of props gets
+//updated or not , if it does'nt then the re-render of this component does not take place
+
+const BigList = React.memo(({ products, addToCart }) => {
+  console.log("biglist called");
   return (
     <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
   );
-};
+});
 
-const SingleProduct = ({ fields }) => {
-  console.log(fields);
+const SingleProduct = ({ fields, addToCart }) => {
+  console.count("single item called");
   let { name, price } = fields;
   price = price / 100;
   const image = fields.image[0].url;
@@ -43,6 +59,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>addToCart</button>
     </article>
   );
 };
