ee815029dad215552b1d9a43d63aec698fc23199
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-04 19:13:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Setup from "../src/setup/index";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
